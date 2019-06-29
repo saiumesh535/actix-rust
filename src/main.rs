aacb94ba6abca7ad99101a37e88083554bd4ba9f
saiumesh-letznav <35692863+saiumesh-letznav@users.noreply.github.com>
2019-06-29 21:19:49 +0530

    db names changed

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ mod commontypes;
 
 
 fn connect_pg() -> PooledConnection<PostgresConnectionManager> {
-    let manager = PostgresConnectionManager::new("postgres://postgres:postgres@localhost:5432/insights",
+    let manager = PostgresConnectionManager::new("postgres://postgres:postgres@localhost:5432/actix-web",
                                                  TlsMode::None).unwrap();
     let pool = r2d2::Pool::new(manager).unwrap();
     let connection = pool.get().unwrap();
